c47be9265d914d8f19757645833654b4c9609561
agustindelmonti <agus.delmonti@gmail.com>
2019-12-03 09:55:59 -0300

    Configuracion modulo material

---
+++ src/app/material.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+
+import {
+  MatButtonModule,
+  MatMenuModule,
+  MatToolbarModule,
+  MatIconModule,
+  MatCardModule
+} from '@angular/material';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    MatButtonModule,
+    MatMenuModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatCardModule
+  ]
+})
+export class MaterialModule { }
