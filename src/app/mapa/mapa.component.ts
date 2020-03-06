87d1954ab73c0574f7d30daa640bbebaaaa15937
agustindelmonti <agus.delmonti@gmail.com>
2020-03-06 11:44:00 -0300

    Refact. codigo de mapa

--- src/app/mapa/mapa.component.ts
+++ src/app/mapa/mapa.component.ts
@@ -31,10 +31,6 @@ export class MapaComponent implements OnInit {
   @ViewChild(CdkVirtualScrollViewport, {static: false}) viewPort: CdkVirtualScrollViewport;
   @ViewChild(MapComponent, {static: false}) map;
 
-  ngAfterViewInit() {
- 
-  }
-
   loading = false;
 
   ngOnInit() {
@@ -52,42 +48,18 @@ export class MapaComponent implements OnInit {
               if (value.interseccion.length > 10) {
                 value.interseccion = value.interseccion.substr(0, 7) + "...";
               }
-              moment.locale('es');
-              value.tiempo = moment([this.formato(value.fecha_hora_estado)], "YYYY, MM, DD, h, mm, ss").fromNow();
-              value.tiempoInterv =moment([this.formato(value.fecha_hora_intervencion)], "YYYY, MM, DD, h, mm, ss").fromNow();
-              value.tiempoMap =moment([this.formato(value.fecha_hora_asignacion)], "YYYY, MM, DD, h, mm, ss").fromNow();
+
+              value.tiempo = this.calculateTime(value.fecha_hora_estado);
+              value.tiempoInterv = this.calculateTime(value.fecha_hora_intervencion);
+              value.tiempoMap = this.calculateTime(value.fecha_hora_asignacion);
           })
           this.solicitudes = data;
           this.loading = false;
-        });
-        this.list = {
-          subtipo: this.rutaActiva.snapshot.params.subtipo,
-        };
-        this.api.getSolicitudes().subscribe(
-          data => {
-            data.forEach(value => {
-              let x = this.reemplazar(this.list.subtipo)
-              if (value.subtipo == x) {
-                console.log(value.subtipo, x);
-                if (value.subtipo.length > 50) {
-                  value.subtipo = value.subtipo.substr(0, 47) + "...";
-                }
-                if (value.descripcion.length > 50) {
-                  value.descripcion = value.descripcion.substr(0, 47) + "...";
-                }
-                if (value.interseccion.length > 10) {
-                  value.interseccion = value.interseccion.substr(0, 7) + "...";
-                }
-                moment.locale('es');
-                value.tiempo = moment([this.formato(value.fecha_hora_estado)], "YYYY, MM, DD, h, mm, ss").fromNow();
-                value.tiempoInterv =moment([this.formato(value.fecha_hora_intervencion)], "YYYY, MM, DD, h, mm, ss").fromNow();
-                value.tiempoMap =moment([this.formato(value.fecha_hora_asignacion)], "YYYY, MM, DD, h, mm, ss").fromNow();
-              this.ar.push(value);
-              }
-            })
-            this.solicitudes = this.ar;
-            this.loading = false;
-          });
+      });
+  }
+
+  calculateTime(date: Date){
+    return moment(date,"DD/MM/YYYY HH:mm:ss").fromNow();
   }
 
   sendto(a, b){
@@ -95,27 +67,14 @@ export class MapaComponent implements OnInit {
     this.router.navigateByUrl(url)
   }
 
-  reemplazar(a){
-    return a.replace("%20", " ")
-  }
   togglePlay(){
     this.asc = !this.asc;
   }
-  formato(fecha){
-    fecha = fecha.replace("/","-")
-    fecha = fecha.replace("/", "-")
-    fecha = fecha.replace(" ", "-")
-    fecha = fecha.replace(":", "-")
-    return fecha.replace(/^(\d{2})-(\d{2})-(\d{4})-(\d{2})-(\d{2})$/g,'$3, $2, $1, $4, $5')
-  }
+
   nextBatch(currIndex: number, items: any[]) {
     const start = this.viewPort.getRenderedRange().start;
     const end = this.viewPort.getRenderedRange().end;
     const total = this.viewPort.getDataLength();
-console.log(`end is ${end} total is ${total}`)
-    if (end == total) {
-      console.log("end reached increase page no")
-    }
   }
 
   trackByIdx(i: number) {
@@ -123,12 +82,10 @@ console.log(`end is ${end} total is ${total}`)
   }
 
   panToSolicitud(x:number, y:number){
-    //console.log("x: " + x + "///y: " + y );
     this.map.moveMap(x,y);
   }
 
   showMe(a:number){
-    //console.log(a + "estoy en mapa");
     this.viewPort.scrollToIndex(a-1);
   }
 }
